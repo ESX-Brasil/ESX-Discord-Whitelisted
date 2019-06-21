7adddae0fd50d85bbe90a821ed6f6d2527ab86b8
renildomarcio <krsolucoesweb@gmail.com>
2019-06-20 23:06:15 -0300

    Update #V1.0.1

--- discord_perms/__resource.lua
+++ discord_perms/__resource.lua
@@ -2,12 +2,14 @@ resource_manifest_version "44febabe-d386-4d18-afbe-5e627f4af937"
 
 description 'ESX Discord Perms'
 
-version '1.0.0'
+version '1.0.1'
 
 server_scripts {
 	"config.lua",
 	"server.lua"
 }
+
+client_script "client.lua"
 
 server_export "IsRolePresent"
 server_export "GetRoles"
