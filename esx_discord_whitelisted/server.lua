9e354b686465bd69c78d9ab56bdd93836e4643af
renildomarcio <krsolucoesweb@gmail.com>
2019-06-20 23:01:26 -0300

    Update server.lua

--- esx_discord_whitelisted/server.lua
+++ esx_discord_whitelisted/server.lua
@@ -1,14 +1,20 @@
 --- Configuração ---
-roleNeeded = "Membro" -- O nome da função necessário para passar a lista de permissões
 notWhitelisted = "Você não está na lista de permissões deste servidor. Entre em nosso discord: COMMUNITY_LINK" -- Mensagem exibida quando não estão na lista de permissões com a função
 noDiscord = "Você deve deixar o Discord aberto para entrar neste servidor." -- Mensagem exibida quando a discord não é encontrada
+
+roles = { -- Role nickname(s) needed to pass the whitelist
+    "Admin",
+    "Whitelisted",
+    "Cidadao",
+}
+
 
 --- Codigo ---
 
 AddEventHandler("playerConnecting", function(name, setCallback, deferrals)
     local src = source
     deferrals.defer()
-    deferrals.update("Verificando Permissões")
+    deferrals.update("Checking Permissions")
 
     for k, v in ipairs(GetPlayerIdentifiers(src)) do
         if string.sub(v, 1, string.len("discord:")) == "discord:" then
@@ -16,13 +22,13 @@ AddEventHandler("playerConnecting", function(name, setCallback, deferrals)
         end
     end
 
-    local allowed = false
     if identifierDiscord then
-        if exports.discord_perms:IsRolePresent(src, roleNeeded) then
-            deferrals.done()
-            allowed = true
-        else
-            deferrals.done(notWhitelisted)
+        for i = 1, #roles do
+            if exports.discord_perms:IsRolePresent(src, roles[i]) then
+                deferrals.done()
+            else
+                deferrals.done(notWhitelisted)
+            end
         end
     else
         deferrals.done(noDiscord)
